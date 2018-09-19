efbf1cb190abd607db07c4df82e47bed88791314
Ross Whitehouse <ross@live.com>
2018-09-19 18:58:54 +0100

    new it

--- script.js
+++ script.js
@@ -197,8 +197,8 @@
     var ret = '';
     ret += '<div id="_hjHTMLErrors">HTML Errors:<br />' +
       'Errors on page: <span id="_hjErrorCount"></span></div>' +
-      '<div>Forms on page: ' + jQuery('form').length + '</div>' +
       '<div>Forms in original source: <span id="_hjSourceForms"></span></div>' +
+      '<div>Forms on page: ' + jQuery('form').length + '</div>' +
       listForms();
     getHTMLErrorCount();
     return ret;
