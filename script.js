178307a2b32164cde4a123f1c7979ba8bc1ab121
Ross Whitehouse <ross@Rosss-MacBook-Pro.local>
2018-09-20 21:04:37 +0100

    new iteration

--- script.js
+++ script.js
@@ -191,12 +191,13 @@
         displayTarget(e.targeting[0]);
       ret += '<li style="color: ';
       ret += isThisPage === 'yes' ? 'green' : 'red';
-      ret += ';"><strong>Correct Page <a href="#" class="_hjFormFieldAttributeButton" style="float: right;">show more</a></strong>' + isThisPage + '</li>';
+      ret += ';"><strong>Correct Page</strong>' + isThisPage + '</li>';
       ret += '<li class="_hjFormFieldAttribute">This shows that the current page is the page that this form should be present on</li>';
       ret += '<li style="color: ';
       ret += isPresent === 'yes' ? 'green' : 'red'
       ret += ';"><strong>Form is present</strong>' + isPresent + '</li>';
-      ret += '<li class="_hjFormFieldAttribute"> This shows whether this form is present on the page</li>';
+      ret += '<li class="_hjFormFieldAttribute">This shows whether this form is present on the page</li>';
+      ret += '<li><a href="#" class="_hjFormFieldAttributeButton" style="float: right;">show info</a></li>';
       jQuery(e.field_info).each(function (fi, fe) {
         ret += '<li class="_hjFormFieldAttribute"><h5>Field ' + (fi + 1) + '</h5></li>' +
           '<li class="_hjFormFieldAttribute"><strong>Type</strong>' + fe.field_type + '</li>' +
