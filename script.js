6165d8e708a2a78b70ae280523d41602ddb1273d
Ross Whitehouse <ross@Rosss-MacBook-Pro.local>
2018-09-21 08:51:08 +0100

    new iteration

--- script.js
+++ script.js
@@ -233,7 +233,7 @@
       ' <li id="_hjJSFormError" style="color: red;"></li>' +
       ' <li><h4>HTML Errors</h4></li>' +
       ' <li id="_hjHTMLErrors"><strong>Errors:</strong> <span id="_hjErrorCount"></span></li>' +
-      ' <li id="_hjKnownIssues" style="color: orange; font-weight: bold;"><h5 id="_hjKnownIssuesCount"></h5></li>' +
+      ' <li id="_hjKnownIssues" style="color: orange;"><h5 id="_hjKnownIssuesCount" style="font-weight: bold;"></h5></li>' +
       '</ul>' +
       listForms();
     getHTMLErrorCount();
